1ba0df4b3d4de476cfa65d8052cf0060c3461b42
Michał Zając <diuf95o@gmail.com>
2021-04-07 17:42:24 +0200

    correct forms.py

--- forms.py
+++ forms.py
@@ -1,8 +1,8 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, TextAreaField, BooleanField
+from wtforms import StringField, TextAreaField, BooleanField, SelectField
 from wtforms.validators import DataRequired
 
 class TodoForm(FlaskForm):
     title = StringField('title', validators=[DataRequired()])
     description = TextAreaField("description", validators=[DataRequired()])
-    done = BooleanField("done", validators=[DataRequired()])
+    done = SelectField('done', choices=[('Tak', True), ('Nie', False)], validators=[DataRequired()])
